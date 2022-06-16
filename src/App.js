a1fdcbcc402714aa075e302b8c45ae9f04308bdc
Hyrondelle <n.hyr@free.fr>
2022-06-16 22:04:59 +0200

    build2

--- src/App.js
+++ src/App.js
@@ -20,18 +20,18 @@ function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="react-blog/">Home</Link>
         
-        {!isAuth?(<Link to="/login">Login</Link>):
+        {!isAuth?(<Link to="react-blog/login">Login</Link>):
         (<>
-          <Link to="/post">Create Post</Link>
+          <Link to="react-blog/post">Create Post</Link>
           <button onClick={signOutUser}>Log Out</button>
         </>)}
       </nav>
       <Routes>
-        <Route path="/" element={<Home isAuth={isAuth}/>}/>
-        <Route path="/login" element={<Login setIsAuth={setIsAuth}/>}/>
-        <Route path="/post" element={<CreatePost isAuth={isAuth}/>}/>
+        <Route path="react-blog/" element={<Home isAuth={isAuth}/>}/>
+        <Route path="react-blog/login" element={<Login setIsAuth={setIsAuth}/>}/>
+        <Route path="react-blog/post" element={<CreatePost isAuth={isAuth}/>}/>
       </Routes>
     </Router>
   );
